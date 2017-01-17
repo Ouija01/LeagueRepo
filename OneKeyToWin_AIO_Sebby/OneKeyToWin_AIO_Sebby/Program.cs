440ad893f1a74eff1df01d05f0c85db7550148ae
OneKeyToWin <kaczor.sebastian@gmail.com>
2017-01-17 10:47:29 +0100

    oktw news

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
@@ -24,7 +24,7 @@ namespace OneKeyToWin_AIO_Sebby
 
         public static Spell Q, W, E, R, Q1, W1, E1, R1;
 
-        private static string OktNews = "Ziggs BETA";
+        private static string OktNews = "stay tuned";
 
         public static AioMode AioModeSet = AioMode.All;
         public static Menu Config;
